e2a909e322bd720e307e2a78981235739b1aea70
Irina Rossokhina <i.rossokhina@yandex.ru>
2023-10-09 18:42:27 +0300

    implemented the function of adding cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
-// @todo: DOM узлы
+let arrOfCards = [];
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function addCard(cardsInfo, deleteFunc) {
+    for (let i = 0; i < cardsInfo.length; i++) {
+        const card = cardTemplate.querySelector('.card').cloneNode(true);
+        const cardImage = card.querySelector('.card__image');
+        const cardTitle = card.querySelector('.card__title');
+        const deleteButton = card.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+        cardImage.src = cardsInfo[i].link;
+        cardImage.alt = cardsInfo[i].name;
+        cardTitle.textContent = cardsInfo[i].name;
+
+        deleteButton.addEventListener('click', function () {
+            const placesItem = deleteButton.closest('.places__item');
+            placesItem.remove();
+        })
+
+        arrOfCards.push(card);
+    }
+
+    return arrOfCards;
+}
+
+function removeCard() {}
+
+addCard(initialCards, removeCard);
+
+arrOfCards.forEach(item => placesList.append(item));
+
+
